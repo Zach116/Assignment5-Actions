22504a2bfc32140a9ab11c716796230edf036db0
Zachary Bishop <zach.bishop116@gmail.com>
2020-02-29 21:43:54 -0800

    circle area

--- task.py
+++ task.py
@@ -1,2 +1,8 @@
+
 def firstrun():
     return "success"
+
+def circleArea(r):
+    pi = 3.14
+    area = pi * (r ** 2)
+    return area
--- tests.py
+++ tests.py
@@ -12,6 +12,17 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+class CircleAreaTest(unittest.TestCase):
+
+    def test1(self):
+        radius = 3
+        expected = 28.26
+        self.assertEqual(expected, task.circleArea(radius))
+
+    def test2(self):
+        radius = 2.5
+        expected = 19.625
+        self.assertEqual(expected, task.circleArea(radius))
 
 if __name__ == '__main__':
     unittest.main()
